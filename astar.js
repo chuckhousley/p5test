170f0af3e7b72af8eb9a44770895474038f8a4bd
Chuck <charles@charleshousley.com>
2015-09-30 22:03:42 -0500

    squares are created and fading properly with 1 width white stroke

--- astar.js
+++ astar.js
@@ -1,48 +1,91 @@
 "use strict";
 var sketch = function(p) {
+    var white, makeAnother;
+    var dimensions = {};
     var grid = [];
+    var emitters = [];
     var squareSize = 15;
-    var numX = p.round(p.windowWidth / squareSize) - 1;
-    var numY = p.round(p.windowHeight / squareSize) - 1;
+
     p.setup = function () {
         p.createCanvas(p.windowWidth, p.windowHeight);
-        for (var x = 0; x < numX; x++) {
-            grid[x] = [];
-            for(var y = 0; y < numY; y++) {
-                grid[x][y] = new Square(x, y);
-            }
-        }
-        p.strokeWeight(1);
+        dimensions = createDimensions();
+        grid = createGrid();
+        white = p.color("white");
+        //pg = p.createGraphics(squareSize, squareSize);
+        emitters.push({x: 20, y: 20, fade: 0.0, color: "#fa6607"});
+        makeAnother = 0;
     };
 
     p.draw = function () {
-        //p.strokeWeight(1);
-        for(var x = 0; x < numX; x++) {
-            for(var y = 0; y < numY; y++) {
-                var current = grid[x][y];
+        p.stroke(255);
+        p.strokeWeight(1);
+        /*for(var x = 0; x < dimensions.numX; x++) {
+            for(var y = 0; y < dimensions.numY; y++) {
+                //var current = grid[x][y];
                 if(p.floor(p.mouseX / squareSize) == x && p.floor(p.mouseY / squareSize) == y) {
-                    p.fill(255, 0, 0);
+                    pg.fill(255, 0, 0);
                 } else {
-                    p.fill(current.color);
+                    pg.fill(255);
                 }
-                p.rect(current.x*squareSize, current.y*squareSize, squareSize, squareSize);
+                pg.rect(0,0,squareSize,squareSize);
+                //pg.rect(x*squareSize, y*squareSize, squareSize, squareSize);
+                p.image(pg, x*squareSize, y*squareSize);
             }
-        }
+        }*/
+        p.fill(255);
+        p.rect(50,25,175,50);
         p.textSize(32);
         p.fill(0,200,0);
         var pos = p.mouseX.toString() + ',' + p.mouseY.toString();
         p.text(pos, 50, 50);
+        fade();
+        makeAnother++;
+
+        if(makeAnother > 10) {
+            emitters.push({x: p.random(5, 25), y: p.random(5, 25), fade: 0.0, color: "#fa6607"});
+            makeAnother = 0;
+        }
     };
 
     p.windowResized = function() {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
+        p.strokeWeight(0);
+        dimensions = createDimensions();
+        grid = createGrid();
+    };
+
+    function createDimensions() {
+        var _dimensions = {};
+        _dimensions.numX = p.round(p.windowWidth / squareSize) - 1;
+        _dimensions.numY = p.round(p.windowHeight / squareSize) - 1;
+        return _dimensions;
     }
 
-    function Square(ix, iy) {
-        this.x = ix;
-        this.y = iy;
-        this.color = p.color(p.random(255));
+    function createGrid() {
+        var _grid = [];
+        for (var x = 0; x < dimensions.numX; x++) {
+            _grid[x] = [];
+            for(var y = 0; y < dimensions.numY; y++) {
+                _grid[x][y] = p.random(10);
+            }
+        }
+        return _grid;
+    }
+    function fade() {
+        var toDelete = 0;
+        for (var i = 0; i < emitters.length; i++) {
+            var e = emitters[i];
+            p.fill(p.lerpColor(p.color(e.color), white, e.fade));
+            p.rect(e.x*squareSize, e.y*squareSize, squareSize, squareSize);
+            e.fade += 0.1;
+            if (e.fade > 1.0) {
+                toDelete++;
 
+            }
+        }
+        for (var j = 0; j < toDelete; j++) {
+            emitters.shift()
+        }
     }
 };
 
