a2ac7b13cd45933cca636d7c7bc17f6cce0a3d30
Chuck <charles@charleshousley.com>
2015-10-09 14:45:50 -0500

    follows mouse

--- astar.js
+++ astar.js
@@ -4,47 +4,32 @@ var sketch = function(p) {
     var dimensions = {};
     var grid = [];
     var emitters = [];
+    var renderQueue = [];
     var squareSize = 15;
 
     p.setup = function () {
         p.createCanvas(p.windowWidth, p.windowHeight);
         dimensions = createDimensions();
         grid = createGrid();
+
         white = p.color("white");
-        //pg = p.createGraphics(squareSize, squareSize);
-        emitters.push({x: 20, y: 20, fade: 0.0, color: "#187b9d"});
         makeAnother = 0;
+        //p.noCursor();
+        p.frameRate(60);  // pc mustard rayse
     };
 
     p.draw = function () {
         p.stroke(255);
-        p.strokeWeight(1);
-        /*for(var x = 0; x < dimensions.numX; x++) {
-            for(var y = 0; y < dimensions.numY; y++) {
-                //var current = grid[x][y];
-                if(p.floor(p.mouseX / squareSize) == x && p.floor(p.mouseY / squareSize) == y) {
-                    pg.fill(255, 0, 0);
-                } else {
-                    pg.fill(255);
-                }
-                pg.rect(0,0,squareSize,squareSize);
-                //pg.rect(x*squareSize, y*squareSize, squareSize, squareSize);
-                p.image(pg, x*squareSize, y*squareSize);
-            }
-        }*/
+        //p.strokeWeight(1);
+        renderQueue.push({x: getX(), y: getY(), fade: 0.0, color: "#bada55"});
+
         p.fill(255);
         p.rect(50,25,175,50);
         p.textSize(32);
         p.fill(0,200,0);
-        var pos = p.mouseX.toString() + ',' + p.mouseY.toString();
+        var pos = getX().toString() + ',' + getY().toString();
         p.text(pos, 50, 50);
         fade();
-        makeAnother++;
-
-        if(makeAnother > 10) {
-            emitters.push({x: p.random(5, 25), y: p.random(5, 25), fade: 0.0, color: "#187b9d"});
-            makeAnother = 0;
-        }
     };
 
     p.windowResized = function() {
@@ -60,7 +45,12 @@ var sketch = function(p) {
         _dimensions.numY = p.round(p.windowHeight / squareSize) - 1;
         return _dimensions;
     }
-
+    function getX() {
+        return p.floor(p.mouseX/squareSize);
+    }
+    function getY() {
+        return p.floor(p.mouseY/squareSize);
+    }
     function createGrid() {
         var _grid = [];
         for (var x = 0; x < dimensions.numX; x++) {
@@ -71,20 +61,22 @@ var sketch = function(p) {
         }
         return _grid;
     }
+    function md(src, dest) {
+        return p.abs(src.x - dest.x) + p.abs(src.y - dest.y);
+    }
     function fade() {
         var toDelete = 0;
-        for (var i = 0; i < emitters.length; i++) {
-            var e = emitters[i];
+        for (var i = 0; i < renderQueue.length; i++) {
+            var e = renderQueue[i];
             p.fill(p.lerpColor(p.color(e.color), white, e.fade));
             p.rect(e.x*squareSize, e.y*squareSize, squareSize, squareSize);
-            e.fade += 0.1;
-            if (e.fade > 1.0) {
+            e.fade += 0.05;
+            if (e.fade > 1.1) { // make extra sure the fade completed
                 toDelete++;
-
             }
         }
         for (var j = 0; j < toDelete; j++) {
-            emitters.shift()
+            renderQueue.shift()
         }
     }
 };
